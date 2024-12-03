76dc5a941c56a2fcc5e30d00e9242718d279b089
khangbkk23 <khang.buitranduycse@hcmut.edu.vn>
2024-12-03 20:11:04 +0700

    UI change

--- backend/processData.js
+++ backend/processData.js
@@ -1,234 +1,241 @@
 class BoyerMoore {
-  buildBadMatchTable(key) {
-    const tableObj = {};
-    const keyLength = key.length;
-    for (let i = 0; i < keyLength - 1; i++) {
-      tableObj[key[i]] = keyLength - 1 - i;
-    }
-    tableObj[key[keyLength - 1]] = keyLength;
-    return tableObj;
-  }
-
-  implement(str, pattern) {
-    const badMatchTable = this.buildBadMatchTable(pattern);
-    let offset = 0;
-    const patternLastIndex = pattern.length - 1;
-    const maxOffset = str.length - pattern.length;
-
-    while (offset <= maxOffset) {
-      let scanIndex = patternLastIndex;
-      while (scanIndex >= 0 && pattern[scanIndex] === str[offset + scanIndex]) {
-        scanIndex--;
+    buildBadMatchTable(key) {
+      const tableObj = {};
+      const keyLength = key.length;
+      for (let i = 0; i < keyLength - 1; i++) {
+        tableObj[key[i]] = keyLength - 1 - i;
       }
-      if (scanIndex < 0) {
-        return offset;
-      }
-      const badMatchChar = str[offset + patternLastIndex];
-      offset += badMatchTable[badMatchChar] || pattern.length;
+      tableObj[key[keyLength - 1]] = keyLength;
+      return tableObj;
     }
-    return -1;
+  
+    implement(str, pattern) {
+      const badMatchTable = this.buildBadMatchTable(pattern);
+      let offset = 0;
+      const patternLastIndex = pattern.length - 1;
+      const maxOffset = str.length - pattern.length;
+  
+      while (offset <= maxOffset) {
+        let scanIndex = patternLastIndex;
+        while (scanIndex >= 0 && pattern[scanIndex] === str[offset + scanIndex]) {
+          scanIndex--;
+        }
+        if (scanIndex < 0) {
+          return offset;
+        }
+        const badMatchChar = str[offset + patternLastIndex];
+        offset += badMatchTable[badMatchChar] || pattern.length;
+      }
+      return -1;
+    }
   }
-}
-
-/*
-    "data_time" : row[0],
-    "trans_no" : parseInt(row[1]),
-    "credit" : parseInt(row[2]),
-    "debit" : parseInt(row[3]),
-    "detail" : row[4]
-*/
-// an OOP class that handle all kind of data query
-class DataHandler {
-  constructor(inputData) {
-    this.inputData = inputData;
-    this.dateComparator = (date1, date2) => {
-      const parseDate = (date) => {
-        const [firstPart, _] = date.split("_");
-        const [day, month, year] = firstPart.split("/");
-        return [parseInt(day), parseInt(month), parseInt(year)];
+  
+  /*
+      "data_time" : row[0],
+      "trans_no" : parseInt(row[1]),
+      "credit" : parseInt(row[2]),
+      "debit" : parseInt(row[3]),
+      "detail" : row[4]
+  */
+  // an OOP class that handle all kind of data query
+  class DataHandler {
+    constructor(inputData) {
+      this.inputData = inputData;
+      this.dateComparator = (date1, date2) => {
+        const parseDate = (date) => {
+          const [firstPart, _] = date.split("_");
+          const [day, month, year] = firstPart.split("/");
+          return [parseInt(day), parseInt(month), parseInt(year)];
+        };
+        const [day1, month1, year1] = parseDate(date1);
+        const [day2, month2, year2] = parseDate(date2);
+        if (year1 != year2) {
+          return year1 - year2;
+        } else if (month1 != month2) {
+          return month1 - month2;
+        } else {
+          return day1 - day2;
+        }
       };
-      const [day1, month1, year1] = parseDate(date1);
-      const [day2, month2, year2] = parseDate(date2);
-      if (year1 != year2) {
-        return year1 - year2;
-      } else if (month1 != month2) {
-        return month1 - month2;
-      } else {
-        return day1 - day2;
+  
+      this.intComparator = (num1, num2) => {
+        return num1 - num2;
+      };
+  
+      this.data = {
+        byDate: {}, // {date_1 : [], date_2: [], .., date_n : [] }
+        byTransNo: {}, // {TransNo_1 : [], TransNo_2: [], .., TransNo_n : [] }
+        byCredit: {}, // ...
+        byDebit: {}, // ..
+      };
+  
+      inputData.forEach((item) => {
+        this.data["byDate"][item.data_time] =
+          this.data["byDate"][item.data_time] ?? [];
+        this.data["byDate"][item.data_time].push(item);
+  
+        this.data["byTransNo"][item.trans_no] =
+          this.data["byTransNo"][item.trans_no] ?? [];
+        this.data["byTransNo"][item.trans_no].push(item);
+  
+        this.data["byCredit"][item.credit] =
+          this.data["byCredit"][item.credit] ?? [];
+        this.data["byCredit"][item.credit].push(item);
+  
+        this.data["byDebit"][item.debit] = 
+          this.data["byDebit"][item.debit] ?? [];
+        this.data["byDebit"][item.debit].push(item);
+      });
+  
+      // support in order retrieval
+      this.keys = {
+        byDate: [], // [date_1, date_2, date_3, ..., date_n]
+        byTransNo: [], // [TransNo_1, TransNo_2, ..., TransNo_n]
+        byCredit: [],
+        byDebit: [],
+      };
+  
+      this.keys["byDate"] = Object.keys(this.data["byDate"])
+        .sort(this.dateComparator);
+      this.keys["byTransNo"] = Object.keys(this.data["byTransNo"])
+        .map(Number)
+        .sort(this.intComparator); // map into number because Object.keys always return string
+      this.keys["byCredit"] = Object.keys(this.data["byCredit"])
+        .map(Number)
+        .sort(this.intComparator);
+      this.keys["byDebit"] = Object.keys(this.data["byDebit"])
+        .map(Number)
+        .sort(this.intComparator);
+  
+      // each element should appear no more than 1
+      this.keys["byDate"] = [... new Set(this.keys["byDate"])];
+      this.keys["byTransNo"] = [... new Set(this.keys["byTransNo"])];
+      this.keys["byCredit"] = [... new Set(this.keys["byCredit"])];
+      this.keys["byDebit"] = [... new Set(this.keys["byDebit"])];
+      
+    }
+  
+    /*
+      return number of entries in entry of type `type`
+    */
+    getSizeOfEntry({ type = undefined }) {
+      if (type == undefined) {
+        return undefined;
       }
-    };
-
-    this.intComparator = (num1, num2) => {
-      return num1 - num2;
-    };
-
-    this.data = {
-      byDate: {}, // {date_1 : [], date_2: [], .., date_n : [] }
-      byTransNo: {}, // {TransNo_1 : [], TransNo_2: [], .., TransNo_n : [] }
-      byCredit: {}, // ...
-      byDebit: {}, // ..
-    };
-
-    inputData.forEach((item) => {
-      this.data["byDate"][item.data_time] =
-        this.data["byDate"][item.data_time] ?? [];
-      this.data["byDate"][item.data_time].push(item);
-
-      this.data["byTransNo"][item.trans_no] =
-        this.data["byTransNo"][item.trans_no] ?? [];
-      this.data["byTransNo"][item.trans_no].push(item);
-
-      this.data["byCredit"][item.credit] =
-      this.data["byCredit"][item.credit] ?? [];
-      this.data["byCredit"][item.credit].push(item);
-
-      this.data["byDebit"][item.debit] = this.data["byDebit"][item.debit] ?? [];
-      this.data["byDebit"][item.debit].push(item);
-    });
-
-    // support in order retrieval
-    this.keys = {
-      byDate: [], // [date_1, date_2, date_3, ..., date_n]
-      byTransNo: [], // [TransNo_1, TransNo_2, ..., TransNo_n]
-      byCredit: [],
-      byDebit: [],
-    };
-
-    this.keys["byDate"] = Object.keys(this.data["byDate"]).sort(
-      this.dateComparator
-    );
-    this.keys["byTransNo"] = Object.keys(this.data["byTransNo"])
-      .map(Number)
-      .sort(this.intComparator); // map into number because Object.keys always return string
-    this.keys["byCredit"] = Object.keys(this.data["byCredit"])
-      .map(Number)
-      .sort(this.intComparator);
-    this.keys["byDebit"] = Object.keys(this.data["byDebit"])
-      .map(Number)
-      .sort(this.intComparator);
-  }
-
-  /*
-    return number of entries in entry of type `type`
-  */
-  getSizeOfEntry({ type = undefined }) {
-    if (type == undefined) {
-      return undefined;
+      return this.keys[type].length;
     }
-    return this.keys[type].length;
-  }
-
-  /*
-    return an array containing items of the `type` type at `index` 
-    indexing by the order of its comparator
-  */
-  getValueInOrder({ index = undefined, type = undefined }) {
-    if (index == undefined || type == undefined) {
-      return undefined;
-    }
-    return this.data[type][this.keys[type][index]];
-  }
-
-  searchByDetail({ key }) {
-    let results = [];
-    let boyerMoore = new BoyerMoore();
-    results = this.inputData.filter(
-      (row) =>
-        boyerMoore.implement(row.detail.toLowerCase(), key.toLowerCase()) != -1
-    );
-    return results;
-  }
-
-  /*
-    return an array of item from `from` to `to` wtih type `type`
-    using the given `comparator` using binary search 
-  */
-  searchInOrder({
-    from = undefined,
-    to = undefined,
-    type = undefined,
-    comparator = () => {},
-  }) {
-    if (type == undefined) {
-      return undefined;
-    }
-    from = from == undefined ? this.keys[type][0] : from;
-    to = to == undefined ? this.keys[type][this.keys[type].length - 1] : to;
-    // a > b => a - b > 0
-    if (comparator(from, to) > 0) {
-      return undefined;
-    }
-    // out of bounds
-    if (comparator(to, this.keys[type][0]) < 0 || comparator(this.keys[type][this.keys[type].length - 1], from) < 0) {
-      return undefined;
-    }
-    let lo = 0,
-      hi = this.keys[type].length - 1;
-    let lower_bound = lo,
-      upper_bound = hi; // the result will contains items in this range
-    while (lo <= hi) {
-      let mid = Math.floor((lo + hi) / 2);
-      // a <= b => a - b <= 0
-      if (comparator(from, this.keys[type][mid]) <= 0) {
-        hi = mid - 1;
-        lower_bound = mid;
-      } else {
-        lo = mid + 1;
+  
+    /*
+      return an array containing items of the `type` type at `index` 
+      indexing by the order of its comparator
+    */
+    getValueInOrder({ index = undefined, type = undefined }) {
+      if (index == undefined || type == undefined) {
+        return undefined;
       }
+      return this.data[type][this.keys[type][index]];
     }
-    (lo = 0), (hi = this.keys[type].length - 1);
-    while (lo <= hi) {
-      let mid = Math.floor((lo + hi) / 2);
-      // a >= b => a - b >= 0
-      if (comparator(to, this.keys[type][mid]) >= 0) {
-        lo = mid + 1;
-        upper_bound = mid;
-      } else {
-        hi = mid - 1;
+  
+    searchByDetail({ key }) {
+      let results = [];
+      let boyerMoore = new BoyerMoore();
+      results = this.inputData.filter(
+        (row) =>
+          boyerMoore.implement(row.detail.toLowerCase(), key.toLowerCase()) != -1
+      );
+      return results;
+    }
+  
+    /*
+      return an array of item from `from` to `to` wtih type `type`
+      using the given `comparator` using binary search 
+    */
+    searchInOrder({
+      from = undefined,
+      to = undefined,
+      type = undefined,
+      comparator = () => {},
+    }) {
+      if (type == undefined) {
+        return undefined;
       }
+      from = from == undefined ? this.keys[type][0] : from;
+      to = to == undefined ? this.keys[type][this.keys[type].length - 1] : to;
+      // a > b => a - b > 0
+      if (comparator(from, to) > 0) {
+        return undefined;
+      }
+      // out of bounds
+      if (comparator(to, this.keys[type][0]) < 0 || comparator(this.keys[type][this.keys[type].length - 1], from) < 0) {
+        return undefined;
+      }
+      let lo = 0,
+        hi = this.keys[type].length - 1;
+      let lower_bound = lo,
+        upper_bound = hi; // the result will contains items in this range
+      while (lo <= hi) {
+        let mid = Math.floor((lo + hi) / 2);
+        // a <= b => a - b <= 0
+        if (comparator(from, this.keys[type][mid]) <= 0) {
+          hi = mid - 1;
+          lower_bound = mid;
+        } else {
+          lo = mid + 1;
+        }
+      }
+      (lo = 0), (hi = this.keys[type].length - 1);
+      while (lo <= hi) {
+        let mid = Math.floor((lo + hi) / 2);
+        // a >= b => a - b >= 0
+        if (comparator(to, this.keys[type][mid]) >= 0) {
+          lo = mid + 1;
+          upper_bound = mid;
+        } else {
+          hi = mid - 1;
+        }
+      }
+      let results = [];
+      for (let i = lower_bound; i <= upper_bound; i++) {
+        this.data[type][this.keys[type][i]].forEach((item) => results.push(item));
+      }
+      return { items: results };
     }
-    let results = [];
-    for (let i = lower_bound; i <= upper_bound; i++) {
-      this.data[type][this.keys[type][i]].forEach((item) => results.push(item));
+  
+    searchByDate({ from = undefined, to = undefined }) {
+      return this.searchInOrder({
+        from: from,
+        to: to,
+        type: "byDate",
+        comparator: this.dateComparator,
+      });
     }
-    return { items: results };
+  
+    searchByTransNo({ from = undefined, to = undefined }) {
+      return this.searchInOrder({
+        from: from,
+        to: to,
+        type: "byTransNo",
+        comparator: this.intComparator,
+      });
+    }
+  
+    searchByCredit({ from = undefined, to = undefined }) {
+      return this.searchInOrder({
+        from: from,
+        to: to,
+        type: "byCredit",
+        comparator: this.intComparator,
+      });
+    }
+  
+    searchByDebit({ from = undefined, to = undefined }) {
+      return this.searchInOrder({
+        from: from,
+        to: to,
+        type: "byDebit",
+        comparator: this.intComparator,
+      });
+    }
   }
-
-  searchByDate({ from = undefined, to = undefined }) {
-    return this.searchInOrder({
-      from: from,
-      to: to,
-      type: "byDate",
-      comparator: this.dateComparator,
-    });
-  }
-
-  searchByTransNo({ from = undefined, to = undefined }) {
-    return this.searchInOrder({
-      from: from,
-      to: to,
-      type: "byTransNo",
-      comparator: this.intComparator,
-    });
-  }
-
-  searchByCredit({ from = undefined, to = undefined }) {
-    return this.searchInOrder({
-      from: from,
-      to: to,
-      type: "byCredit",
-      comparator: this.intComparator,
-    });
-  }
-
-  searchByDebit({ from = undefined, to = undefined }) {
-    return this.searchInOrder({
-      from: from,
-      to: to,
-      type: "byDebit",
-      comparator: this.intComparator,
-    });
-  }
-}
-
-module.exports = { DataHandler };
+  
+  module.exports = { DataHandler };
--- frontend/src/components/pages/Contact.js
+++ frontend/src/components/pages/Contact.js
@@ -1,29 +1,54 @@
-import React from 'react';
+import React from 'react'
 
 const Contact = () => {
-    return (
-        <div className="min-h-screen p-8 ">           
-            <div className="bg-white p-6 shadow-md rounded-lg max-w-lg mx-auto">
-                <h1 className="text-3xl font-bold text-center text-blue-600 mb-6">Liên hệ với chúng tôi</h1>
-                <p className="text-center text-gray-700 mb-8">
-                    Bạn có câu hỏi hoặc muốn biết thêm thông tin về dự án? Hãy liên hệ với chúng tôi qua các phương thức dưới đây.
-                </p>
-                <h2 className="text-2xl font-semibold text-blue-500 mb-4">Thông tin liên hệ</h2>
-                <div className="mb-4">
-                    <p className="text-lg font-semibold">Email: </p>
-                    <a href="mailto:bao.dangnguyen@hcmut.edu.vn" className="text-blue-500 hover:underline">bao.dangnguyen@hcmut.edu.vn</a>
-                </div>
-                <div className="mb-4">
-                    <p className="text-lg font-semibold">Số điện thoại: </p>
-                    <p className="text-blue-500">0912.345.678</p>
-                </div>
-                <div className="mb-4">
-                    <p className="text-lg font-semibold">GitHub: </p>
-                    <a href="https://github.com/bibimoni" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">bibimoni</a>
-                </div>
-            </div>
+  return (
+    <div className='min-h-screen p-8 '>
+      <div className='bg-white p-6 shadow-md rounded-lg max-w-lg mx-auto'>
+        <h1 className='text-3xl font-bold text-center text-blue-600 mb-6'>
+          Liên hệ với chúng tôi
+        </h1>
+        <p className='text-center text-gray-700 mb-8'>
+          Bạn có câu hỏi hoặc muốn biết thêm thông tin về dự án? Hãy liên hệ với
+          chúng tôi qua các phương thức dưới đây.
+        </p>
+        <h2 className='text-2xl font-semibold text-blue-500 mb-4'>
+          Thông tin liên hệ
+        </h2>
+        <div className='mb-4'>
+          <p className='text-lg font-semibold'>Email: </p>
+          <a
+            href='mailto:bao.dangnguyen@hcmut.edu.vn'
+            className='text-blue-500 hover:underline'
+          >
+            bao.dangnguyen@hcmut.edu.vn
+          </a>
         </div>
-    );
-};
+        <div className='mb-4'>
+          <p className='text-lg font-semibold'>Số điện thoại: </p>
+          <a
+            href='https://zalo.me/0905191826'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='text-blue-500 hover:underline'
+          >
+            0905.191.826
+          </a>
+        </div>
 
-export default Contact;
+        <div className='mb-4'>
+          <p className='text-lg font-semibold'>GitHub: </p>
+          <a
+            href='https://github.com/bibimoni'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='text-blue-500 hover:underline'
+          >
+            bibimoni
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
